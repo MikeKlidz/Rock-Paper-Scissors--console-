0e0e1e8885e98921df23c09019c0c5c12f2b5cbf
Mikhail Klidzhan <johniehuge@gmail.com>
2022-07-17 21:21:09 +0300

    Created a function to sum up the results of the game

--- index.js
+++ index.js
@@ -37,10 +37,37 @@ function playRound(playerChoice = prompt("Enter 'Rock', 'Paper' or 'Scissors'...
 // Function to play 5-round game
 
 function game(){
+    
+    let wins = [];
+    let losses = [];
     for(let i = 0; i < 5; i++){
-        console.log(playRound());
+        let round = playRound();
+        if(round === "You win!"){
+            wins.push("You win");
+        }
+        if(round === "You lose!"){
+            losses.push("You lose");
+        }
+
+        console.log(round);
+    }
+// Function to sum up the game
+    function summary(){
+        console.log(wins, losses);
+        if(wins.length === losses.length){
+            return "You drew the game!";
+        }
+        else if(wins.length > losses.length) {
+            return "You have won the game!";
+        }
+        else {
+            return "You have lost the game!"
+        }
+        
     }
     
+
+console.log(summary());
 }
 
 game();
