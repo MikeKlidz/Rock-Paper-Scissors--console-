57169b6ac77dc5c8f9484d9bf289dcb86c6538b4
Mikhail Klidzhan <johniehuge@gmail.com>
2022-07-17 20:31:06 +0300

    Created a function for one round of the game

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const gameOptions = ["Rock", "Paper", "Scissors"];
 
+
 function getComputerChoice(){
     const randomNumber = Math.round(Math.random() * 2);
     const compChoice = gameOptions[randomNumber];
     return compChoice;
 }
 
-console.log(getComputerChoice());
+function playRound(playerChoice = prompt("Enter 'Rock', 'Paper' or 'Scissors'..."), compChoice = getComputerChoice()) {
+    let firstLetter = playerChoice.slice(0, 1).toUpperCase();
+    let restOfWord =  playerChoice.slice(1, playerChoice.length).toLowerCase();
+    const playerSelection = firstLetter.concat(restOfWord);
+    
+    if(playerSelection !== gameOptions[0] && playerSelection !== gameOptions[1] && playerSelection !== gameOptions[2]) return "Enter a valid word!";
+    
+    if(playerSelection === compChoice) return "You draw!";
+    
+    if(playerSelection === "Rock" && compChoice === "Scissors") 
+    return "You win!";
+    
+    if(playerSelection === "Paper" && compChoice === "Rock") 
+    
+    return "You win!"
+    
+    if(playerSelection === "Scissors" && compChoice === "Paper") 
+    return "You win!";
+    
+    else {
+        return "You lose!";
+    }
+}
+console.log(playRound());
